a368bd1c914f379be3f0e8afb9ce288d9d0c2988
zakhy <z.lyrshikov@cybercode.pro>
2025-08-24 16:15:13 +0700

    api docs

--- services/notification-api/src/api/v1/api.py
+++ services/notification-api/src/api/v1/api.py
@@ -1,44 +1,60 @@
 from uuid import UUID
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
+from shared.models.base import BaseResponse
 from shared.models.notification import NotificationCreate, NotificationEvent, \
     NotificationTemplate
 
 router = APIRouter(prefix="/api/v1", tags=["Notification API V1"])
 
 
-@router.get("/")
-async def root():
-    return {"message": "Notification API"}
-
-
-@router.post("/notifications/send", description="Отправка рассылок")
-async def send_notification(notification: NotificationCreate):
+@router.post("/notifications/send", description="Отправка рассылок",
+             responses={
+                 status.HTTP_201_CREATED: {
+                     "message": "Successfully sent",
+                     "model": BaseResponse
+                 }
+             }, status_code=201)
+async def send_notification(notification: NotificationCreate) -> BaseResponse:
     pass
 
 
-@router.post("/notifications/events", description="Прием событий")
-async def send_event(event: NotificationEvent):
+@router.post("/notifications/events", description="Прием событий",
+             responses={
+                 status.HTTP_201_CREATED: {
+                     "message": "Successfully sent",
+                     "model": BaseResponse
+                 }
+             }, status_code=201)
+async def send_event(event: NotificationEvent) -> BaseResponse:
     pass
 
 
 @router.get("/notifications/user/{user_id}",
             description="Получение уведомлений пользователя")
-async def send_notification_to_user(user_id: UUID):
+async def send_notification_to_user(user_id: UUID) -> BaseResponse:
     pass
 
 
-@router.get("/templates", description="Получение списка шаблонов")
+@router.get("/templates",
+            response_model=list[NotificationTemplate],
+            description="Получение списка шаблонов")
 async def get_templates() -> list[NotificationTemplate]:
     pass
 
 
-@router.post("/templates", description="Создание шаблона")
-async def create_template(template: NotificationTemplate):
+@router.post("/templates", description="Создание шаблона",
+             responses={
+                 status.HTTP_201_CREATED: {
+                     "message": "Successfully sent",
+                     "model": BaseResponse
+                 }
+             }, status_code=201)
+async def create_template(template: NotificationTemplate) -> BaseResponse:
     pass
 
 
 @router.get("/health", description="Health Check")
-async def health_check():
-    return {"status": "ok"}
+async def health_check() -> BaseResponse:
+    return BaseResponse(success=True, message="Ok")
